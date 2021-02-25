72926d6bb8a3878c209ab2afb4c9167ba2a32687
branabm <zujalica810@gmail.com>
2021-02-25 13:32:05 +0100

    ispravka

--- cypress/integration/loginTest1.spec.js
+++ cypress/integration/loginTest1.spec.js
@@ -6,8 +6,29 @@ describe('login test', ()=> {
         cy.get("a[href='/login']").click()
 }) 
      it("email input is empty", ()=> {
-     cy.get('#email').type('');
-     cy.get('#password').type('12345678');
-     cy.get('button').click();
+     cy.get('#email').type('').clear()
+     cy.get('#password').type('12345678').clear()
+     cy.get('button').click()
 })
-})
+    it("password input is empty", ()=> {
+        cy.get('#email').type('branatest@test.com').clear();
+        cy.get('#password').type('').clear()
+        cy.get('button').click()
+        })
+
+        it('wrong email', ()=> {
+            cy.get('#email').type('brnanaana@test.com').clear()
+            cy.get('#password').type('12345678').clear()
+            cy.get('button').click()
+        })
+        it('wrong password', ()=> {
+            cy.get('#email').type('branatest@test.com').clear()
+            cy.get('#password').type('12311245').clear()
+            cy.get('button').click()
+        })
+        it('wrong password', ()=> {
+            cy.get('#email').type('').clear()
+            cy.get('#password').type('').clear()
+            cy.get('button').click()
+        })    
+ })
